69abb3a3c91f90e6659b5e734035d644158bcef0
Ada Salazar <AdaSalazarLl@gmail.com>
2013-01-31 19:20:03 +0000

    banner divs & gradient stuff

--- knewu/index.php
+++ knewu/index.php
@@ -19,58 +19,6 @@
 	
 
 	<div id="wrapper">
-
-		<?php
-			/*$labels = array("Home","MakeUp Basics","Gallery","Tutorials");
-			$link = array("index","makeupBasics","gallery","tutorials");
-			//$labels = array("","","","");
-			$currentFile = $_SERVER["PHP_SELF"];
-			$parts = Explode('/', $currentFile);
-			//this will just take the name of the file   start  end
-			$tabName =  substr($parts[count($parts) - 1],  0,   -4);
-			echo '
-				<div id="tabs">';
-			*/
-			/*for ($i=0; $i<=3; $i++){
-			//echo $link[$i].'  '.$tabName;
-				if (strcmp ($link[$i],$tabName) == 0){
-					echo '<div id="selected">';
-				} else {
-					echo '<div id="noSelected">';
-				}				
-				echo '<a href="'.$link[$i].'.php?adminEntryVerified">'.$labels[$i].'</a></div>';
-			 }	
-			echo 	'<div id="noSelected">
-							<!-- This is the search engine miniform-->
-						<form action="../cms/search.php" method="GET">
-									<input type="text" size="15" name="search">
-									<input type="submit" name="submit" value="Search">
-						</form>
-					</div>
-			</div><!--tabs-->
-				';*/
-			 
-			
-			//echo $tabName;
-			/*	echo '
-			<div id="tabs"> 
-				<ul>
-					<li id="selected"><a href="../cms/index.php?adminEntryVerified">Home</a></li>
-					<li><a href="../cms/viewProducts.php?adminEntryVerified">Makeup Basics</a></li>
-					<li><a href="../cms/lowStockProducts.php?adminEntryVerified">Gallery</a></li>
-					<li><a href="../cms/manageAdmin.php?adminEntryVerified">Tutorials</a></li>
-					<li>
-							<!-- This is the search engine miniform-->
-						<form action="../cms/search.php" method="GET">
-									<input type="text" size="15" name="search">
-									<input type="submit" name="submit" value="Search">
-						</form>
-					</li>
-				</ul>
-			</div><!--tabs-->
-				';*/
-
-		?>
 		<div id="content">
 
 			<p>asdf
