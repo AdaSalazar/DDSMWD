0c26bcdda2e01831b0ebdc8212e1d558e7218809
Ada Salazar <AdaSalazarLl@gmail.com>
2013-02-14 13:03:12 +0000

    Professinal working

--- knewu/makeupBasics.php
+++ knewu/makeupBasics.php
@@ -14,11 +14,30 @@
 	<script type="text/javascript" src="../files/javascripts/styleswitcher.js"></script>
 </head>
 
-<body>
-	<!-- Places the images on top-->
-	<?php require 'cms/banner.php'?>
-	<div id="wrapper">
-		<?php require 'cms/sesionStarter.php'?>
+<body><div id="wrapper"><div id="topBanner">
+			<div id="leftImg"><img src="images/banner+logo.png" alt="Right brush image not available"></div>
+			<!--<div id="rightImg"><img class="brush" src="images/RBrush.png" align="right" alt="Right brush image not available"></div>
+			<div id="centerImg"><img class="logo" src="images/Logo.png"  alt="Logo image not available"></div>
+			<div id="leftImg"><img class="brush" src="images/LBrush.png" alt="Left brush image not available"></div>-->
+			<div id="rightImg">
+				<a href="http://www.youtube.com/user/AbiSalazarPro"><img src="images/youtube.png" height="25px" width="25px"></a>
+				<a href="https://twitter.com/AbiSalazar1"><img src="images/twitter.png" height="25px" width="25px"></a>
+				<a href="http://www.flickr.com/photos/abisalazarpro/"><img src="images/flickr.png" height="25px" width="25px">	</a>
+				<a href="https://www.facebook.com/AbiSalazarPro"><img src="images/fb.png" height="25px" width="25px"></a>	
+			</div>		
+		</div>
+		<!-- creates the top menu & bottom -->				
+			<div id="right">
+							<div id="tabs"><div id="noSelected"><a href="index.php?adminEntryVerified">Home</a></div><div id="selected"><a href="makeupBasics.php?adminEntryVerified">MakeUp Basics</a></div><div id="noSelected"><a href="gallery.php?adminEntryVerified">Gallery</a></div><div id="noSelected"><a href="tutorials.php?adminEntryVerified">Tutorials</a></div><div id="noSelected">
+									<div id="sForm">
+											<!-- This is the search engine miniform-->
+										<form action="search.php" method="GET">
+													<input type="text" size="15" name="search" class="searchBox">
+													<input type="submit" name="submit" value="Search" class="searchButton">
+										</form>
+									</div>
+								</div>
+						</div><!--tabs-->
 		<div id="content">
 
 			<p>asdf
