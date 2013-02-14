9b82709c94f60ba7b6541f898312bdb83da7c1fb
Ada Salazar <AdaSalazarLl@gmail.com>
2013-02-14 14:19:08 +0000

    Professional Look Fixed

--- knewu/tutorials.php
+++ knewu/tutorials.php
@@ -1,94 +1,90 @@
-<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
-
+    
+<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head>
-	<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-	<meta http-equiv="Default-Style" content="Main">
-	<title>Tutorials - kNewU</title>
-	<!--This part does the styles switcher-->
-	<link rel="stylesheet" type="text/css" title="Main" 
-	href="css/main.css">
-	<link rel="alternate stylesheet" type="text/css" title="High Contrast" 
-	href="css/highContrast.css">
-	<script type="text/javascript" src="../files/javascripts/styleswitcher.js"></script>
-</head>
-
-<body><div id="wrapper"><div id="topBanner">
-			<div id="leftImg"><img src="images/banner+logo.png" alt="Right brush image not available"></div>
-			<!--<div id="rightImg"><img class="brush" src="images/RBrush.png" align="right" alt="Right brush image not available"></div>
-			<div id="centerImg"><img class="logo" src="images/Logo.png"  alt="Logo image not available"></div>
-			<div id="leftImg"><img class="brush" src="images/LBrush.png" alt="Left brush image not available"></div>-->
-			<div id="rightImg">
-				<a href="http://www.youtube.com/user/AbiSalazarPro"><img src="images/youtube.png" height="25px" width="25px"></a>
-				<a href="https://twitter.com/AbiSalazar1"><img src="images/twitter.png" height="25px" width="25px"></a>
-				<a href="http://www.flickr.com/photos/abisalazarpro/"><img src="images/flickr.png" height="25px" width="25px">	</a>
-				<a href="https://www.facebook.com/AbiSalazarPro"><img src="images/fb.png" height="25px" width="25px"></a>	
-			</div>		
-		</div>
-<!-- creates the top menu & bottom -->				
-			<div id="right">
-							<div id="tabs"><div id="noSelected"><a href="index.php?adminEntryVerified">Home</a></div><div id="noSelected"><a href="makeupBasics.php?adminEntryVerified">MakeUp Basics</a></div><div id="noSelected"><a href="gallery.php?adminEntryVerified">Gallery</a></div><div id="selected"><a href="tutorials.php?adminEntryVerified">Tutorials</a></div><div id="noSelected">
-									<div id="sForm">
-											<!-- This is the search engine miniform-->
-										<form action="search.php" method="GET">
-													<input type="text" size="15" name="search" class="searchBox">
-													<input type="submit" name="submit" value="Search" class="searchButton">
-										</form>
-									</div>
-								</div>
-						</div><!--tabs-->
-		<div id="content">
-
-			<p>asdf
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				end
-			</p>
-		</div><!--content-->
-	</div><!--wrapper-->
-	
-	<br>
-	<br>
-	<div id="footer">	
-		<p><a>&copy; Ada Salazar 2011 - Present</a>
-		&nbsp;|&nbsp;
-		<a href="disclaimer.html"> Disclaimer</a>
-		&nbsp;|&nbsp;
-		<a href="#topOfPage">Top of page</a></p>
-	</div><!--footer-->
-</body>
-
+  <head>	
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">	
+    <meta http-equiv="Default-Style" content="Main">	
+    <title>Tutorials - kNewU
+    </title>	
+    <!--This part does the styles switcher-->	
+    <link rel="stylesheet" type="text/css" title="Main"  	href="css/main.css">	
+    <link rel="alternate stylesheet" type="text/css" title="High Contrast"  	href="css/highContrast.css">	
+<script type="text/javascript" src="../files/javascripts/styleswitcher.js"></script>
+  </head>
+  <body>
+    <div id="wrapper">
+      <div id="topBanner">			
+        <div id="leftImg">
+          <img src="images/banner+logo.png" alt="Right brush image not available">
+        </div>			
+        <!--<div id="rightImg"><img class="brush" src="images/RBrush.png" align="right" alt="Right brush image not available"></div>
+        			<div id="centerImg"><img class="logo" src="images/Logo.png"  alt="Logo image not available"></div>
+        			<div id="leftImg"><img class="brush" src="images/LBrush.png" alt="Left brush image not available"></div>-->			
+        <div id="rightImg">				
+          <a href="http://www.youtube.com/user/AbiSalazarPro">
+            <img src="images/youtube.png" height="25px" width="25px"></a>				
+          <a href="https://twitter.com/AbiSalazar1">
+            <img src="images/twitter.png" height="25px" width="25px"></a>				
+          <a href="http://www.flickr.com/photos/abisalazarpro/">
+            <img src="images/flickr.png" height="25px" width="25px">	</a>				
+          <a href="https://www.facebook.com/AbiSalazarPro">
+            <img src="images/fb.png" height="25px" width="25px"></a>	 			
+        </div>		 		
+      </div>
+      <!-- creates the top menu & bottom -->				 			
+      <div id="right">							
+        <div id="tabs">
+          <div id="noSelected">
+            <a href="index.php?adminEntryVerified">Home</a>
+          </div>
+          <div id="noSelected">
+            <a href="makeupBasics.php?adminEntryVerified">MakeUp Basics</a>
+          </div>
+          <div id="noSelected">
+            <a href="gallery.php?adminEntryVerified">Gallery</a>
+          </div>
+          <div id="selected">
+            <a href="tutorials.php?adminEntryVerified">Tutorials</a>
+          </div>
+          <div id="noSelected">									
+            <div id="sForm">											
+              <!-- This is the search engine miniform-->										
+              <form action="search.php" method="GET">													
+                <input type="text" size="15" name="search" class="searchBox">													
+                <input type="submit" name="submit" value="Search" class="searchButton">										
+              </form>									
+            </div>								
+          </div>								
+        </div>						
+      </div>
+      <!--tabs-->		
+      <div id="content">			 
+        <p>Content  				           
+          <br>				           
+          <br>				asdfasdfasfsdf 				           
+          <br>				           
+          <br>				           
+          <br>				           
+          <br>				           
+          <br>				           
+          <br>				           
+          <br>				           
+          <br>				           
+          <br>				 				 				           
+          <br>				end 			         
+        </p>		       
+      </div>
+      <!--content-->	
+    </div>
+    <!--wrapper-->	 	
+    <br>	
+    <br>	
+    <div id="footer">	 		
+      <p><a>&copy; Ada Salazar 2011 - Present</a>		&nbsp;|&nbsp; 		
+        <a href="disclaimer.html"> Disclaimer</a>		&nbsp;|&nbsp; 		
+        <a href="#topOfPage">Top of page</a>
+      </p>	
+    </div>
+    <!--footer-->
+  </body>
 </html> 
